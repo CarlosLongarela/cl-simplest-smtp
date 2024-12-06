06ff07f6f3a74f56447343a990f9694af836a3bf
Carlos Longarela <carlos@longarela.eu>
2024-12-07 00:55:17 +0100

    Agregar etiquetas y placeholders a los campos de configuración SMTP en la página de ajustes

--- includes/templates/settings-page.php
+++ includes/templates/settings-page.php
@@ -29,39 +29,51 @@ $from_name  = get_option( 'cl_simplest_smtp_from_name' );
 		<table class="form-table">
 			<tbody>
 				<tr>
-					<th scope="row"><?php esc_html_e( 'SMTP Server', 'cl-simplest-smtp' ); ?></th>
+					<th scope="row">
+						<label for="cl_simplest_smtp_server"><?php esc_html_e( 'SMTP Server', 'cl-simplest-smtp' ); ?></label>
+					</th>
 					<td>
-						<input type="text" name="cl_simplest_smtp_server" value="<?php echo esc_attr( $server ); ?>" class="regular-text">
+						<input type="text" name="cl_simplest_smtp_server" value="<?php echo esc_attr( $server ); ?>" placeholder="smtp.test.null" required class="regular-text">
 					</td>
 				</tr>
 				<tr>
-					<th scope="row"><?php esc_html_e( 'SMTP Port', 'cl-simplest-smtp' ); ?></th>
+					<th scope="row">
+						<label for="cl_simplest_smtp_port"><?php esc_html_e( 'SMTP Port', 'cl-simplest-smtp' ); ?></label>
+					</th>
 					<td>
-						<input type="text" name="cl_simplest_smtp_port" value="<?php echo esc_attr( $port ); ?>" class="regular-text">
+						<input type="text" name="cl_simplest_smtp_port" value="<?php echo esc_attr( $port ); ?>" class="regular-text" placeholder="465" required autocomplete="off" />
 					</td>
 				</tr>
 				<tr>
-					<th scope="row"><?php esc_html_e( 'SMTP Username', 'cl-simplest-smtp' ); ?></th>
+					<th scope="row">
+						<label for="cl_simplest_smtp_username"><?php esc_html_e( 'SMTP Username', 'cl-simplest-smtp' ); ?></label>
+					</th>
 					<td>
-						<input type="text" name="cl_simplest_smtp_username" value="<?php echo esc_attr( $username ); ?>" class="regular-text">
+						<input type="text" name="cl_simplest_smtp_username" value="<?php echo esc_attr( $username ); ?>" class="regular-text" placeholder="smptuser@fakedomain.null" autocomplete="off" />
 					</td>
 				</tr>
 				<tr>
-					<th scope="row"><?php esc_html_e( 'SMTP Password', 'cl-simplest-smtp' ); ?></th>
+					<th scope="row">
+						<label for="cl_simplest_smtp_password"><?php esc_html_e( 'SMTP Password', 'cl-simplest-smtp' ); ?></label>
+					</th>
 					<td>
-						<input type="password" name="cl_simplest_smtp_password" value="<?php echo esc_attr( $password ); ?>" class="regular-text">
+						<input type="password" name="cl_simplest_smtp_password" value="<?php echo esc_attr( $password ); ?>" class="regular-text" placeholder="youruserfakepass" autocomplete="off" />
 					</td>
 				</tr>
 				<tr>
-					<th scope="row"><?php esc_html_e( 'From Email', 'cl-simplest-smtp' ); ?></th>
+					<th scope="row">
+						<label for="cl_simplest_smtp_from_email"><?php esc_html_e( 'From Email', 'cl-simplest-smtp' ); ?></label>
+					</th>
 					<td>
-						<input type="email" name="cl_simplest_smtp_from_email" value="<?php echo esc_attr( $from_email ); ?>" class="regular-text">
+						<input type="email" name="cl_simplest_smtp_from_email" value="<?php echo esc_attr( $from_email ); ?>" class="regular-text" placeholder="manolito@guorpresser.null" autocomplete="off" />
 					</td>
 				</tr>
 				<tr>
-					<th scope="row"><?php esc_html_e( 'From Name', 'cl-simplest-smtp' ); ?></th>
+					<th scope="row">
+						<label for="cl_simplest_smtp_from_name"><?php esc_html_e( 'From Name', 'cl-simplest-smtp' ); ?></label>
+					</th>
 					<td>
-						<input type="text" name="cl_simplest_smtp_from_name" value="<?php echo esc_attr( $from_name ); ?>" class="regular-text">
+						<input type="text" name="cl_simplest_smtp_from_name" value="<?php echo esc_attr( $from_name ); ?>" class="regular-text" placeholder="Manolito Guorpresser" autocomplete="off" />
 					</td>
 				</tr>
 			</tbody>
