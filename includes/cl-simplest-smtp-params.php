38c149c29d7b2fb8c61fe752067fbf8a12f469da
Carlos Longarela <carlos@longarela.eu>
2024-12-07 02:40:00 +0100

    Refactor la obtención de parámetros SMTP para manejar valores vacíos y mejorar la legibilidad del código

--- includes/cl-simplest-smtp-params.php
+++ includes/cl-simplest-smtp-params.php
@@ -19,12 +19,71 @@ if ( ! defined( 'CL_SIMPLEST_SMTP_HOST' ) ) {
 	// If the constant is not defined, get ALL the values from the plugin options.
 	$cl_simplest_smtp_data = get_option( 'cl_simplest_smtp_options' );
 
-	define( 'CL_SIMPLEST_SMTP_HOST', esc_attr( $cl_simplest_smtp_data['host'] ) );
-	define( 'CL_SIMPLEST_SMTP_AUTH', filter_var( $cl_simplest_smtp_data['auth'], FILTER_VALIDATE_BOOLEAN ) );
-	define( 'CL_SIMPLEST_SMTP_PORT', filter_var( $cl_simplest_smtp_data['port'], FILTER_VALIDATE_INT ) );
-	define( 'CL_SIMPLEST_SMTP_USER', esc_attr( $cl_simplest_smtp_data['user'] ) );
-	define( 'CL_SIMPLEST_SMTP_PASS', esc_attr( $cl_simplest_smtp_data['pass'] ) );
-	define( 'CL_SIMPLEST_SMTP_SECURE', esc_attr( $cl_simplest_smtp_data['secure'] ) );
-	define( 'CL_SIMPLEST_SMTP_FROM', filter_var( $cl_simplest_smtp_data['from'], FILTER_VALIDATE_EMAIL ) );
-	define( 'CL_SIMPLEST_SMTP_NAME', esc_attr( $cl_simplest_smtp_data['name'] ) );
+	if ( empty( $cl_simplest_smtp_data ) ) {
+		$cl_simplest_smtp_host   = '';
+		$cl_simplest_smtp_auth   = false;
+		$cl_simplest_smtp_port   = '';
+		$cl_simplest_smtp_user   = '';
+		$cl_simplest_smtp_pass   = '';
+		$cl_simplest_smtp_secure = '';
+		$cl_simplest_smtp_from   = '';
+		$cl_simplest_smtp_name   = '';
+	} else {
+		if ( isset( $cl_simplest_smtp_data['host'] ) ) {
+			$cl_simplest_smtp_host = esc_attr( $cl_simplest_smtp_data['host'] );
+		} else {
+			$cl_simplest_smtp_host = '';
+		}
+
+		if ( isset( $cl_simplest_smtp_data['auth'] ) ) {
+			$cl_simplest_smtp_auth = filter_var( $cl_simplest_smtp_data['auth'], FILTER_VALIDATE_BOOLEAN );
+		} else {
+			$cl_simplest_smtp_auth = false;
+		}
+
+		if ( isset( $cl_simplest_smtp_data['port'] ) ) {
+			$cl_simplest_smtp_port = filter_var( $cl_simplest_smtp_data['port'], FILTER_VALIDATE_INT );
+		} else {
+			$cl_simplest_smtp_port = '';
+		}
+
+		if ( isset( $cl_simplest_smtp_data['user'] ) ) {
+			$cl_simplest_smtp_user = esc_attr( $cl_simplest_smtp_data['user'] );
+		} else {
+			$cl_simplest_smtp_user = '';
+		}
+
+		if ( isset( $cl_simplest_smtp_data['pass'] ) ) {
+			$cl_simplest_smtp_pass = esc_attr( $cl_simplest_smtp_data['pass'] );
+		} else {
+			$cl_simplest_smtp_pass = '';
+		}
+
+		if ( isset( $cl_simplest_smtp_data['secure'] ) ) {
+			$cl_simplest_smtp_secure = esc_attr( $cl_simplest_smtp_data['secure'] );
+		} else {
+			$cl_simplest_smtp_secure = '';
+		}
+
+		if ( isset( $cl_simplest_smtp_data['from'] ) ) {
+			$cl_simplest_smtp_from = filter_var( $cl_simplest_smtp_data['from'], FILTER_VALIDATE_EMAIL );
+		} else {
+			$cl_simplest_smtp_from = '';
+		}
+
+		if ( isset( $cl_simplest_smtp_data['name'] ) ) {
+			$cl_simplest_smtp_name = esc_attr( $cl_simplest_smtp_data['name'] );
+		} else {
+			$cl_simplest_smtp_name = '';
+		}
+	}
+
+	define( 'CL_SIMPLEST_SMTP_HOST', $cl_simplest_smtp_host );
+	define( 'CL_SIMPLEST_SMTP_AUTH', $cl_simplest_smtp_auth );
+	define( 'CL_SIMPLEST_SMTP_PORT', $cl_simplest_smtp_port );
+	define( 'CL_SIMPLEST_SMTP_USER', $cl_simplest_smtp_user );
+	define( 'CL_SIMPLEST_SMTP_PASS', $cl_simplest_smtp_pass );
+	define( 'CL_SIMPLEST_SMTP_SECURE', $cl_simplest_smtp_secure );
+	define( 'CL_SIMPLEST_SMTP_FROM', $cl_simplest_smtp_from );
+	define( 'CL_SIMPLEST_SMTP_NAME', $cl_simplest_smtp_name );
 }
